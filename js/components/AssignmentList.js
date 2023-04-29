438a7a378920edf11e892d4ae7b811332f9e3124
hadimakki387 <hmakki387@gmail.com>
2023-04-29 21:44:58 +0300

    edited some styling and hide toggle

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -2,48 +2,54 @@ import Assignment from "./Assignment.js";
 import AssignmentTags from "./AssignmentTags.js";
 
 export default {
-  components: { Assignment, AssignmentTags },
+    components: { Assignment, AssignmentTags },
 
-  template: `
-        <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">
-            {{ title }}
-            <span>({{assignments.length}})</span>
-            </h2>
-            <AssignmentTags
-              v-model:currentTag="currentTag" 
-              :initTags="assignments.map(a=>a.tag)"
-            />
-            <ul 
-              class="border border-gray-600 divide-y divide-gray-600 mt-6"
-              
-              >
+    template: `
+        <section v-show="assignments.length" class="w-65">
+            <div class="flex justify-between items-start">
+                <h2 class="font-bold mb-2">
+                    {{ title }}
+                    <span>({{ assignments.length }})</span>
+                </h2>
+                
+                <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
+            </div>
+            
+            <assignment-tags
+                v-model:currentTag="currentTag"
+                :initial-tags="assignments.map(a => a.tag)" 
+            ></assignment-tags>
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                <assignment 
-                    v-for="assignment in fiteredAssignments"
+                    v-for="assignment in filteredAssignments"
                     :key="assignment.id" 
                     :assignment="assignment"
                 ></assignment>
             </ul>
+            
+            <slot></slot>
         </section> 
     `,
-//in the assignment component this is where the data is being synced on the main side
-//fina nsammiha lli badna ye v-model:name="...", w mnistilima honik 3a hal 2ases
-  props: {
-    assignments: Array,
-    title: String,
-  },
-  data() {
-    return {
-      currentTag: "all",
-    };
-  },
-  computed: {
-    fiteredAssignments() {
-      if (this.currentTag === "all") {
-        return this.assignments;
-      }
 
-      return this.assignments.filter((a) => a.tag === this.currentTag);
+    props: {
+        assignments: Array,
+        title: String,
+        canToggle: { type: Boolean, default: false }
     },
-  },
-};
+
+    data() {
+        return {
+            currentTag: 'all',
+        };
+    },
+
+    computed: {
+        filteredAssignments() {
+            if (this.currentTag === 'all') {
+                return this.assignments;
+            }
+
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        }
+    }
+}
